78f990fc683c55349ae52606edc20a007d64aeab
Dilip Krishnan <d_krishnan@containerstore.com>
2015-04-01 15:13:28 -0500

    Fixed the static resource handler problem that prevented
    spring boot from doing its WebMvcAutoConfiguration magic. More details on the fix
    are available at this commit https://github.com/springfox/springfox/commit/5464e516c75b1586e06734d91b86f595e7d39b2d

--- swagger2/src/main/java/springfoxdemo/swagger2/Application.java
+++ swagger2/src/main/java/springfoxdemo/swagger2/Application.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.swagger.ui.configuration.SwaggerUiConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import springfox.petstore.controller.PetController;
 
 @SpringBootApplication
 @EnableSwagger2
+@Import(SwaggerUiConfiguration.class)
 @ComponentScan(basePackageClasses = PetController.class)
 public class Application {
     public static void main(String[] args) {
