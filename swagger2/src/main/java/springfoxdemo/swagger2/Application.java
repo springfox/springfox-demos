5b17aee37339bc99851aadee0a4b1fc1dab43922
Adrian Kelly <adrianbkelly@gmail.com>
2015-04-08 00:28:11 +1000

    WIP to display available swagger resources

--- swagger2/src/main/java/springfoxdemo/swagger2/Application.java
+++ swagger2/src/main/java/springfoxdemo/swagger2/Application.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger.ui.ApiResourceLocator;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import springfox.petstore.controller.PetController;
 
 @SpringBootApplication
 @EnableSwagger2
-@ComponentScan(basePackageClasses = PetController.class)
+@ComponentScan(basePackageClasses = {
+        PetController.class,
+        ApiResourceLocator.class
+})
 public class Application {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
