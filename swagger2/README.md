7774efe7f3c68de08ff2b5e6f9075da4e9eeecc6
Adrian Kelly <adrianbkelly@gmail.com>
2015-04-11 01:47:12 +1000

    Using latests swagger-ui webjar

--- swagger2/README.md
+++ swagger2/README.md
@@ -5,5 +5,8 @@
 ```
 
 ## Paths
-Swagger endpoint: http://localhost:8080/swagger2/v2/api-docs
-Swagger UI endpoint: http://localhost:8080/swagger2/webjars/springfox-swagger-ui/2.0.0-SNAPSHOT/sdoc.jsp
+All Swagger Resources(groups) http://localhost:8080/swagger2/swagger-resources
+Swagger UI endpoint: http://localhost:8080/swagger2/swagger-ui.html
+Swagger docs endpoint: http://localhost:8080/swagger2/v2/api-docs
+
+
--- swagger2/src/main/java/springfoxdemo/swagger2/Application.java
+++ swagger2/src/main/java/springfoxdemo/swagger2/Application.java
@@ -4,15 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import springfox.documentation.swagger.ui.ApiResourceLocator;
+import springfox.documentation.swagger.ui.EnableSwaggerUi;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import springfox.petstore.controller.PetController;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableSwaggerUi
 @ComponentScan(basePackageClasses = {
         PetController.class,
-        ApiResourceLocator.class
 })
 public class Application {
     public static void main(String[] args) {
