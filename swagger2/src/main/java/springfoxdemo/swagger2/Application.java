7dc38dd3d5c3e25ea29c20e159e42c31c8c664ad
Adrian Kelly <adrianbkelly@gmail.com>
2015-04-11 23:49:24 +1000

    Fleshing out demo a bit further

--- swagger2/src/main/java/springfoxdemo/swagger2/Application.java
+++ swagger2/src/main/java/springfoxdemo/swagger2/Application.java
@@ -7,17 +7,25 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.AuthorizationScopeBuilder;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.BasicAuth;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.annotations.EnableSwagger;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import springfox.petstore.controller.PetController;
 
 import static com.google.common.base.Predicates.or;
+import static com.google.common.collect.Lists.newArrayList;
 import static springfox.documentation.builders.PathSelectors.regex;
 
 @SpringBootApplication
-@EnableSwagger2
+@EnableSwagger //Enable swagger 1.2 spec
+@EnableSwagger2 //Enable swagger 2.0 spec
 @ComponentScan(basePackageClasses = {
         PetController.class,
 })
@@ -36,10 +44,22 @@ public class Application {
                 .build();
     }
 
-
     @Bean
     public Docket userApi() {
+        AuthorizationScope[] authScopes = new AuthorizationScope[1];
+        authScopes[0] = new AuthorizationScopeBuilder()
+                .scope("read")
+                .description("read access")
+                .build();
+        SecurityReference auth = SecurityReference.builder()
+                .reference("test")
+                .scopes(authScopes)
+                .build();
+
         return new Docket(DocumentationType.SWAGGER_2)
+                .securitySchemes(newArrayList(new BasicAuth("test")))
+                .securityContext(SecurityContext.builder()
+                        .withAuthorizations(newArrayList(auth)).build())
                 .groupName("user-api")
                 .apiInfo(apiInfo())
                 .select()
