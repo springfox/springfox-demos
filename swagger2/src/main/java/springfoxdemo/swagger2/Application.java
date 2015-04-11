054df6448c90a83405efe067f0292186187e36bd
Adrian Kelly <adrianbkelly@gmail.com>
2015-04-11 16:50:38 +1000

    Updating demo to showcase swagger groups

--- swagger2/src/main/java/springfoxdemo/swagger2/Application.java
+++ swagger2/src/main/java/springfoxdemo/swagger2/Application.java
@@ -1,50 +1,85 @@
 package springfoxdemo.swagger2;
 
+import com.google.common.base.Predicate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import springfox.documentation.builders.AuthorizationScopeBuilder;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.BasicAuth;
-import springfox.documentation.service.SecurityReference;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.annotations.EnableSwagger;
-import springfox.documentation.swagger.ui.EnableSwaggerUi;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import springfox.petstore.controller.PetController;
 
-import static com.google.common.collect.Lists.*;
+import static com.google.common.base.Predicates.or;
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @SpringBootApplication
-@EnableSwagger
 @EnableSwagger2
-@EnableSwaggerUi
 @ComponentScan(basePackageClasses = {
-    PetController.class
+        PetController.class,
 })
 public class Application {
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
-  }
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+    }
 
-  @Bean
-  public Docket docket() {
-    AuthorizationScope[] authScopes = new AuthorizationScope[1];
-    authScopes[0] = new AuthorizationScopeBuilder()
-        .scope("read")
-        .description("read access")
-        .build();
-    SecurityReference auth = SecurityReference.builder()
-        .reference("test")
-        .scopes(authScopes)
-        .build();
-    return new Docket(DocumentationType.SWAGGER_2)
-        .securitySchemes(newArrayList(new BasicAuth("test")))
-        .securityContext(SecurityContext.builder()
-            .withAuthorizations(newArrayList(auth))
-            .build());
-  }
+    @Bean
+    public Docket petApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("full-petstore-api")
+                .apiInfo(apiInfo())
+                .select()
+                .paths(petstorePaths())
+                .build();
+    }
+
+
+    @Bean
+    public Docket userApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("user-api")
+                .apiInfo(apiInfo())
+                .select()
+                .paths(userOnlyEndpoints())
+                .build();
+    }
+
+    private Predicate<String> petstorePaths() {
+        return or(
+                regex("/api/pet.*"),
+                regex("/api/user.*"),
+                regex("/api/store.*")
+        );
+    }
+
+    private Predicate<String> userOnlyEndpoints() {
+        return new Predicate<String>() {
+            @Override
+            public boolean apply(String input) {
+                return input.contains("user");
+            }
+        };
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Springfox petstore API")
+                .description("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum " +
+                        "has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a " +
+                        "galley of type and scrambled it to make a type specimen book. It has survived not only five " +
+                        "centuries, but also the leap into electronic typesetting, remaining essentially unchanged. " +
+                        "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum " +
+                        "passages, and more recently with desktop publishing software like Aldus PageMaker including " +
+                        "versions of Lorem Ipsum.")
+                .termsOfServiceUrl("http://springfox.io")
+                .contact("springfox")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://github.com/springfox/springfox/blob/master/LICENSE")
+                .version("2.0")
+                .build();
+    }
+
 }
