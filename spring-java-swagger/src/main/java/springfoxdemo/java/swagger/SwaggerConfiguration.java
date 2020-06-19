e6cb2de55738baeca3f78340bb1ad56f24404d7d
Dilip Krishnan <dilipkrish_subscriptions@yahoo.com>
2020-06-18 22:40:20 -0500

    Legacy Mvc Apps work

--- spring-java-swagger/src/main/java/springfoxdemo/java/swagger/SwaggerConfiguration.java
+++ spring-java-swagger/src/main/java/springfoxdemo/java/swagger/SwaggerConfiguration.java
@@ -1,8 +1,8 @@
 package springfoxdemo.java.swagger;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
-@EnableSwagger2
+@EnableSwagger2WebMvc
 public class SwaggerConfiguration {
 
 }
--- spring-xml-swagger/src/main/java/springfoxdemo/xml/swagger/ApplicationSwaggerConfig.java
+++ spring-xml-swagger/src/main/java/springfoxdemo/xml/swagger/ApplicationSwaggerConfig.java
@@ -1,8 +1,8 @@
 package springfoxdemo.xml.swagger;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
-@EnableSwagger2
+@EnableSwagger2WebMvc
 public class ApplicationSwaggerConfig {
 
 }
