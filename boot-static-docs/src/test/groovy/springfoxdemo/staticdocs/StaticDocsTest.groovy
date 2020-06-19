1c54fe23950ca419bf923986d7f49e71ed920cdf
Dilip Krishnan <dilipkrish_subscriptions@yahoo.com>
2020-06-18 22:07:27 -0500

    Upgraded the library versions

--- boot-static-docs/src/test/groovy/springfoxdemo/staticdocs/StaticDocsTest.groovy
+++ boot-static-docs/src/test/groovy/springfoxdemo/staticdocs/StaticDocsTest.groovy
@@ -2,56 +2,43 @@ package springfoxdemo.staticdocs
 
 import groovy.io.FileType
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.SpringApplicationContextLoader
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.MediaType
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.TestExecutionListeners
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener
-import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import org.springframework.web.context.WebApplicationContext
 import spock.lang.Ignore
+import spock.lang.Specification
 import springfox.documentation.staticdocs.Swagger2MarkupResultHandler
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
-@ContextConfiguration(
-        loader = SpringApplicationContextLoader,
-        classes = Application)
-@WebAppConfiguration
-@TestExecutionListeners([DependencyInjectionTestExecutionListener, DirtiesContextTestExecutionListener])
-class StaticDocsTest extends spock.lang.Specification {
+@AutoConfigureMockMvc
+@WebMvcTest
+class StaticDocsTest extends Specification {
 
   @Autowired
-  WebApplicationContext context;
-
   MockMvc mockMvc;
-
-  def setup() {
-    this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build()
-  }
 
   @Ignore
   def "generates the petstore api asciidoc"() {
     setup:
-      String outDir = System.getProperty('asciiDocOutputDir', 'build/aciidoc')
-      Swagger2MarkupResultHandler resultHandler = Swagger2MarkupResultHandler
-              .outputDirectory(outDir)
-              .build()
+    String outDir = System.getProperty('asciiDocOutputDir', 'build/aciidoc')
+    Swagger2MarkupResultHandler resultHandler = Swagger2MarkupResultHandler
+        .outputDirectory(outDir)
+        .build()
 
     when:
-      this.mockMvc.perform(get("/v2/api-docs").accept(MediaType.APPLICATION_JSON))
-              .andDo(resultHandler)
-              .andExpect(status().isOk())
+    this.mockMvc.perform(get("/v2/api-docs").accept(MediaType.APPLICATION_JSON))
+        .andDo(resultHandler)
+        .andExpect(status().isOk())
+
     then:
-      def list = []
-      def dir = new File(resultHandler.outputDir)
-      dir.eachFileRecurse(FileType.FILES) { file ->
-        list << file.name
-      }
-      list.sort() == ['definitions.adoc', 'overview.adoc', 'paths.adoc']
+    def list = []
+    def dir = new File(resultHandler.outputDir)
+    dir.eachFileRecurse(FileType.FILES) { file ->
+      list << file.name
+    }
+    list.sort() == ['definitions.adoc', 'overview.adoc', 'paths.adoc']
   }
 }
