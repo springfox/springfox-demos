6d0545c622389ae8fa2f0e15b919f97742efd7df
Dilip Krishnan <dilipkrish_subscriptions@yahoo.com>
2020-07-05 09:24:43 -0500

    Working

--- spring-java-swagger/src/main/java/springfoxdemo/java/swagger/AppConfiguration.java
+++ spring-java-swagger/src/main/java/springfoxdemo/java/swagger/AppConfiguration.java
@@ -1,13 +1,20 @@
 package springfoxdemo.java.swagger;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @Configuration
-@ComponentScan
 @EnableWebMvc
+@ComponentScan("springfoxdemo.java.swagger")
 @EnableSwagger2WebMvc
-public class ApplicationConfiguration {
+public class AppConfiguration {
+  @Bean
+  public Docket docket() {
+    return new Docket(DocumentationType.SWAGGER_2);
+  }
 }
--- spring-java-swagger/src/main/java/springfoxdemo/java/swagger/SpringConfig.java
+++ spring-java-swagger/src/main/java/springfoxdemo/java/swagger/SpringConfig.java
@@ -1,17 +1,27 @@
 package springfoxdemo.java.swagger;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
+@Component
 public class SpringConfig implements WebMvcConfigurer {
-
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("swagger-ui.html")
-        .addResourceLocations("classpath:/META-INF/resources/");
+    registry.
+        addResourceHandler("/swagger-ui/**")
+        .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
+        .resourceChain(false);
+  }
 
-    registry.addResourceHandler("/webjars/**")
-        .addResourceLocations("classpath:/META-INF/resources/webjars/");
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/swagger-ui/")
+        .setViewName("forward:" + "/swagger-ui/index.html");
   }
 }
